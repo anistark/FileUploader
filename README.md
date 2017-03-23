0575faf021732cbc7685e288a5cfae6a4d9a4c03
Kumar Anirudha <anirudhastark@yahoo.com>
2017-03-24 04:30:07 +0530

    button added

--- README.md
+++ README.md
@@ -6,3 +6,23 @@ React Native App
 
 ## Under Development
 
+
+## App Directory Structure
+
+    App
+    ├── Components
+    │   └── Styles
+    ├── Config
+    ├── Containers
+    │   └── Styles
+    ├── Fixtures
+    ├── Images
+    ├── Lib
+    ├── Navigation
+    │   └── Styles
+    ├── Redux
+    ├── Sagas
+    ├── Services
+    ├── Themes
+    └── Transforms
+
--- index.android.js
+++ index.android.js
@@ -3,10 +3,44 @@ import {
     AppRegistry,
     StyleSheet,
     Text,
-    View
+    View,
+    TextInput,
+    ScrollView,
+    TouchableNativeFeedback
 } from 'react-native';
 
-export default class FileUploader extends Component {
+//export default class FileUploader extends Component {
+//    render() {
+//        return (
+//            <View style={styles.container}>
+//                <Text style={styles.title}>
+//                    File Uploader
+//                </Text>
+//                <Text style={styles.subTitle}>
+//                    Upload Files Fast and Ease
+//                </Text>
+//                <Text style={styles.body}>
+//                    Kumar Anirudha{'\n'}
+//                    anistark anirudha.org
+//                </Text>
+//            </View>
+//        );
+//    }
+//}
+//
+//
+//AppRegistry.registerComponent('FileUploader', () => FileUploader);
+
+class FileUploader extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {text: ''};
+    }
+
+    _onPressButton() {
+        console.log("You tapped the button!");
+    }
+
     render() {
         return (
             <View style={styles.container}>
@@ -16,38 +50,44 @@ export default class FileUploader extends Component {
                 <Text style={styles.subTitle}>
                     Upload Files Fast and Ease
                 </Text>
-                <Text style={styles.body}>
-                    Kumar Anirudha{'\n'}
-                    anistark anirudha.org
-                </Text>
+                <TouchableNativeFeedback
+                    onPress={this._onPressButton}
+                    background={TouchableNativeFeedback.SelectableBackground()}>
+                    <View style={{width: 100, height: 100, backgroundColor: '#47AFF1'}}>
+                        <Text style={{margin: 30}}>Button</Text>
+                    </View>
+                </TouchableNativeFeedback>
             </View>
         );
     }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#AFECCA'
-  },
-  title: {
-    fontSize: 40,
-    textAlign: 'center',
-    color: '#95ABB9',
-    margin: 10
-  },
-  subTitle: {
-    textAlign: 'center',
-    color: '#95ABB9',
-    marginBottom: 5
-  },
-  body: {
-    textAlign: 'center',
-    color: '#47AFF1',
-    marginBottom: 5
-  }
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#AFECCA'
+    },
+    title: {
+        fontSize: 40,
+        textAlign: 'center',
+        color: '#95ABB9',
+        margin: 10
+    },
+    subTitle: {
+        textAlign: 'center',
+        color: '#95ABB9',
+        marginBottom: 5
+    },
+    body: {
+        textAlign: 'center',
+        color: '#47AFF1',
+        marginBottom: 5
+    },
+    fileUploadButton: {
+        backgroundColor: '#26C1C1'
+    }
 });
 
 AppRegistry.registerComponent('FileUploader', () => FileUploader);
