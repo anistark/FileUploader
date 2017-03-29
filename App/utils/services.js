e87c9e68768867bba62f48bdfc9704605e7abb0b
Kumar Anirudha <anirudhastark@yahoo.com>
2017-03-30 03:03:21 +0530

    all file types supported

--- App/utils/services.js
+++ App/utils/services.js
@@ -2,8 +2,6 @@
  * Created by Anirudha on 26/03/17.
  */
 
-var mime = require('mime');
-
 var helper = require('./helper');
 
 var baseUrl = 'http://f873b56d.ngrok.io/api/v1';
@@ -15,30 +13,32 @@ exports.uploadFile = async function (file, cb) {
         var file_path_arr = file_path.split("/");
         var file_name = file_path_arr[file_path_arr.length-1];
         console.log(file_name);
-        console.log(mime.lookup(file_name));
-        //console.log(helper.getExtension(file_name_arr[1]));
-        //var input_file = {
-        //    uri: file.uri,
-        //    type: 'image/png',
-        //    name: file_name
-        //};
-        //var formData = new FormData();
-        //formData.append('input_file', input_file);
-        //var fetchingUrl = baseUrl+'/upload';
-        //let response = await fetch(fetchingUrl, {
-        //    method: 'POST',
-        //    headers: {
-        //        Accept: 'application/json'
-        //    },
-        //    body: formData
-        //});
-        //let responseJson = await response.json();
-        //console.log(responseJson);
-        //cb(responseJson);
-        cb({
-            'success': true,
-            'message': 'success'
-        });
+        var fileType = helper.mimeTypeLookup(file_name);
+        if (fileType) {
+            var input_file = {
+                uri: file.uri,
+                type: fileType,
+                name: file_name
+            };
+            var formData = new FormData();
+            formData.append('input_file', input_file);
+            var fetchingUrl = baseUrl+'/upload';
+            let response = await fetch(fetchingUrl, {
+                method: 'POST',
+                headers: {
+                    Accept: 'application/json'
+                },
+                body: formData
+            });
+            let responseJson = await response.json();
+            console.log(responseJson);
+            cb(responseJson);
+        }
+        else {
+            console.log('File Type Not supported yet!');
+            // Todo Record the file name. Discover the extension later.
+            cb(null, 'File type not supported yet. The issue has been reported and the said file type will soon be addressed.');
+        }
     } catch(error) {
         console.log('Error in Network Request to File Upload Server');
         console.log(error);
--- android/app/src/main/java/com/fileuploader/MainActivity.java
+++ android/app/src/main/java/com/fileuploader/MainActivity.java
@@ -1,22 +1,21 @@
 package com.fileuploader;
 
+import com.filepicker.FilePickerPackage; // Lichwa/react-native-file-picker
 import com.facebook.react.ReactActivity;
 
-import com.filepicker.FilePickerPackage; // Lichwa/react-native-file-picker
-
 public class MainActivity extends ReactActivity {
-
-    @Override
-    protected List<ReactPackage> getPackages() {
-        return Arrays.<ReactPackage>asList(
-                new MainReactPackage(),
-                new FilePickerPackage() // Add package
-        );
-    }
 
     @Override
     protected String getMainComponentName() {
         return "FileUploader";
     }
+
+//    @Override
+//    protected List<ReactPackage> getPackages() {
+//        return Arrays.<ReactPackage>asList(
+//                new MainReactPackage(),
+//                new FilePickerPackage() // Add package
+//        );
+//    }
 
 }
