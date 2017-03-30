2948a84f8c03bf47cb42e4341e7fabe156e7afb5
Kumar Anirudha <anirudhastark@yahoo.com>
2017-03-31 02:27:54 +0530

    ios design set.

---
+++ App/screens/home_ios.js
@@ -0,0 +1,57 @@
+/**
+ * Created by Anirudha on 25/03/17.
+ */
+
+import React, { Component } from 'react';
+import {
+    AppRegistry,
+    View,
+    Text,
+    AlertIOS,
+    TouchableOpacity
+} from 'react-native';
+
+var styles = require('../styles/home');
+var services = require('../utils/services');
+
+//const DocumentPicker = require('react-native').NativeModules.RNDocumentPicker;
+
+class HomeScreen extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {file: ''};
+    }
+
+    _onPressButton() {
+        console.log('Button Pressed');
+        AlertIOS.alert(
+            'Button Clicked'
+        );
+        //DocumentPicker.show({
+        //    filetype: ['public.image'],
+        //},(error,url) => {
+        //    alert(url);
+        //});
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.title}>
+                    File Uploader
+                </Text>
+                <Text style={styles.subTitle}>
+                    Upload Files Fast and Ease
+                </Text>
+                <TouchableOpacity
+                    onPress={this._onPressButton}>
+                    <View style={styles.buttonRoundStyle}>
+                        <Text style={styles.buttonTextStyle}>Upload</Text>
+                    </View>
+                </TouchableOpacity>
+            </View>
+        );
+    }
+}
+
+module.exports = HomeScreen;
--- android/app/src/main/java/com/fileuploader/MainApplication.java
+++ android/app/src/main/java/com/fileuploader/MainApplication.java
@@ -3,6 +3,7 @@ package com.fileuploader;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.azendoo.reactnativesnackbar.SnackbarPackage;
 import com.filepicker.FilePickerPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -24,6 +25,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new SnackbarPackage(),
             new FilePickerPackage()
       );
     }
